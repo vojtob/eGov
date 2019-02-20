eb97b14541274bdab9938f339c893bc17873a5fd
Vojto Bálint <38936748+vojtob@users.noreply.github.com>
2019-02-20 09:41:35 +0100

    Update memos.bat

--- utils/memos.bat
+++ utils/memos.bat
@@ -1,6 +1,3 @@
 set PROJECT_DIR=%~dp0
 set PROJECT_DIR=%PROJECT_DIR:~0,-7%
-
-mkdir %PROJECT_DIR%\Memos
-
-copy %PROJECT_DIR%\Architecture_src\memos\*.* %PROJECT_DIR%\Memos
+call C:\Projects_src\Personal\Archi-img\utils\memos.bat %PROJECT_DIR%
